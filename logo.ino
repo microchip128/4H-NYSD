7e7152bb5e29437f7b174f1137189a38356698af
Jeff Branson <jeffbranson65@gmail.com>
2017-03-29 19:04:15 -0600

    Add files via upload

---
+++ logo.ino
@@ -0,0 +1,45 @@
+void logo()
+
+{
+htmlText.concat("MMMMMMMMMMMMMMMMMmhyssssssyhdNMMMMMMMMMMMMMMMMMMMmdyssssssyhdNMMMMMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMMMMhysssssssssssssydMMMMMMMMMMMMMMNhyssssssssssssshNMMMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMMmysssssssssssssssssydMMMMMMMMMMNhssssssssssssssssssdMMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMmysssssssssssssssssssshMMMMMMMMdssssssssssssssssssssshMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMdysssssssssssssssssssssshMMMMMMhsssssssssssssssssssssssymMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMNmhyssssssso:.+ssssssssssssssdMMMMmssssssssssssss+.-ossssssssydNMMMMMMMMM");
+htmlText.concat("MMMMMmhyssssssssss-   .+ssssssssssssyNMMMhssssssssssss+.   -sssssssssssyhNMMMMMM");
+htmlText.concat("MMMmyssssssssssssss+-   .+sssssssssssdMMMsssssssssss+.   -+ssssssssssssssshNMMMM");
+htmlText.concat("MNhssssssssso:/ssssso.    .+ssssssssshMMMsssssssss+.    .+sssss/:osssssssssymMMM");
+htmlText.concat("Mdssssssssso`  ./so:` `:-   .+ssssssssNMMsssssss+.   -:` `-os/.  `+sssssssssyNMM");
+htmlText.concat("Mdsssssssssso-   `` `:oss+.  `+sssssssNMmsssssso.  -+sso/` ``   -osssssssssssNMM");
+htmlText.concat("Mdsssssssssssso-    :ssssss+/ossssssssNMdssssssss/+ssssss/    -osssssssssssssNMM");
+htmlText.concat("Mmssssssssssssss+-   ./sssssssssssssssmMdssssssssssssss/.   -+ssssssssssssssyNMM");
+htmlText.concat("MMhsssssssssssssss+-   -sssssssssssssshMhsssssssssssss-   -+sssssssssssssssymMMM");
+htmlText.concat("MMMdssssssssssssssss+-:ossssssssssssssyhsssssssssssssso/-+ssssssssssssssssymMMMM");
+htmlText.concat("MMMMmyssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssyhNMMMMM");
+htmlText.concat("MMMMMNmhyssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssyhdNMMMMMMM");
+htmlText.concat("MMMMMMMMNNddhyyyysssssssssssssssssssssssssssssssssssssssssssssyyyhhdmNMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMMMNNNNNNNmddddddhyssssssssssssssssssyhddddddddNNNNNNNMMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMNNmmmddddddddddddddyssssssssssssssssssyddddddddddddddddmmNMMMMMMMMMMMM");
+htmlText.concat("MMMMMMNdhyyysssssssssssssssssssssssssssssssssssssssssssssssssssssssyyhmNMMMMMMMM");
+htmlText.concat("MMMNdhyssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssyhdNMMMMM");
+htmlText.concat("MMmyssssssssssssssssosssssssssssssssssssssssssssssssssssssosssssssssssssssydMMMM");
+htmlText.concat("Mdssssssssssssssso/.`:ssssssssssssssssssssssssssssssssss+.`:ossssssssssssssshNMM");
+htmlText.concat("dssssssssssssss+-`  `:osssssssssssssssssssssssssssssssss/.  `-+sssssssssssssshMM");
+htmlText.concat("sssssssssssso/.   ./ossssssssssssssymysssdhsssssssssssssss+-   ./ossssssssssssmM");
+htmlText.concat("sssssssssso:`     :ssssso/:sssssssshMyssyMNssssssss+:osssss+`    `-+ssssssssssdM");
+//htmlText.concat("yssssssss/`  `:+-  .+s+-`  -sssssssNMyssyMMyssssss/`  -+so:  `//.   -sssssssssdM");
+htmlText.concat("msssssssss/-+ssss/`  .   ./ossssssyMMyssyMMdsssssss+-`  .` `:osss+--ossssssssyNM");
+htmlText.concat("Mmysssssssssssssso-   `-+sssssssssdMNysssNMNssssssssso:`   .+ssssssssssssssyhmhm");
+htmlText.concat("MMNmyysssssssss+-   `:ossssssssssyNMmssssmMMhsssssssssss/.   ./ssssssssssyhmdhyh");
+htmlText.concat("MMMMMNdysssssss:``./sssssssssssssdMMmsssshMMNyssssssssssss+-` -ossssssshmNMmhhmM");
+htmlText.concat("MMMMMMMNhsssssssoossssssssssssssyMMMmssssyNMMmyssssssssssssso+sssssssymMNdddNMMM");
+htmlText.concat("MMMMMMMMMhsssssssssssssssssssssyNMMMMyssssdMMMmysssssssssssssssssssssmMmyyymMMMM");
+htmlText.concat("MMMMMMMMMMhssssssssssssssssssshNMMMMMyssssyNMMMNyssssssssssssssssssymMmhhdNMMMMM");
+htmlText.concat("MMMMMMMMMMMhsssssssssssssssshmMMMMMMMdssssshMMMMMdysssssssssssssssymmydNNMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMmhyssssssssssydNMMMMMMMMMNyssssshMMMMMMmhyssssssssssydNmhhmMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMMMMNdddhhddmNMMMMMMMMMMMMMhssssssdMMMMMMMMNdddhhddNNMMMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMyssssssdMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmssssssshMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+htmlText.concat("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMmysssssshNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+}
